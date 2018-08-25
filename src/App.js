b311c97207c953681b94f8c75590aec278df2dec
Berkay Askin <berkayaskiin@gmail.com>
2018-08-25 08:43:16 +0000

    fetch users from api, create parent component of cardlist called scroll

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import {robots} from './robots';
+import Scroll from './Scroll';
 import './App.css';
 
 
@@ -9,26 +9,39 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
-        }
+        };
+    }
+    
+    componentDidMount () {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response =>response.json())
+        .then(users =>this.setState({robots:users}));
     }
     
     onSearchChange = (event) => {
-        this.setState({searchfield: event.target.value})
+        this.setState({searchfield: event.target.value});
     }
     
     render() {
         const filteredRobots = this.state.robots.filter(robots =>{
-           return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+           return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         });
+        if(this.state.robots.length === 0) {
+            return <h1>Loading...</h1>
+        } else {
         return (
+            
         <div className='tc'>
             <h1 className='f1'>RoboFriends</h1>
             <SearchBox searchChange={this.onSearchChange} />
-            <CardList robots= {filteredRobots} /> 
+            <Scroll>
+                <CardList robots= {filteredRobots} /> 
+            </Scroll>
         </div>
-    );
+        );
+    }
   }
 }
 
