2a251c36af4b65f50fd5c34851ac81627e6318af
Berkay Askin <berkayaskiin@gmail.com>
2018-08-27 20:11:07 +0000

    add redux middleware, async actions

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,45 +6,39 @@ import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
-import { setSearchField } from '../actions';
+import { setSearchField, requestRobots } from '../actions';
 
+// parameter state comes from index.js provider store state(rootReducers)
 const mapStateToProps = state => {
     return {
-        searchField: state.searchField
+        searchField: state.searchRobots.searchField,
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending
     }
 }
+
+// dispatch the DOM changes to call an action. note mapStateToProps returns object, mapDispatchToProps returns function
+// the function returns an object then uses connect to change the data from reducers.
 const mapDispatchToProps = (dispatch) => {
     return {
-        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+        onRequestRobots: () => dispatch(requestRobots())
     }    
 }
 
-
-const fetch = require('isomorphic-fetch');
-
 class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            robots: [],
-            
-        };
-    }
     
     componentDidMount () {
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response =>response.json())
-        .then(users =>this.setState({robots:users}));
+        this.props.onRequestRobots();
     }
     
     render() {
-        const {robots} = this.state;
-        const {searchField, onSearchChange} = this.props;
+        const {searchField, onSearchChange, robots, isPending} = this.props;
         const filteredRobots = robots.filter(robot =>{
            return robot.name.toLowerCase().includes(searchField.toLowerCase());
         });
         
-        return !robots.length ? /*robots.length === 0*/
+        return isPending ? 
            <h1>Loading...</h1> :
            (
             <div className='tc'>
@@ -58,4 +52,5 @@ class App extends Component {
         }
     }
 
+// action done from mapDispatchToProps will channge state from mapStateToProps
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
-import { searchRobots } from './reducers';
+import { searchRobots, requestRobots } from './reducers';
 import 'tachyons';
 
-const store = createStore(searchRobots);
+const logger = createLogger();
 
-ReactDOM.render(
-                <Provider store={store}>
-                    <App />
-                </Provider>, document.getElementById('root'));
+const rootReducer = combineReducers({ searchRobots, requestRobots});
+
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
+
+
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
